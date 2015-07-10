88bec995b4cd4efe447c47c2d65c49d5c8d44355
Gaurav Langan <gauravlangan@gmail.com>
2015-07-10 10:45:18 +0800

    final P1

--- P1/Problem Set 3/linear_regression.py
+++ P1/Problem Set 3/linear_regression.py
@@ -12,7 +12,7 @@ In this question, you need to:
 def linear_regression(features, values):
     """
     Perform linear regression given a data set with an arbitrary number of features.
-
+    
     This can be the same code as in the lesson #3 exercise.
     """
     features = sm.add_constant(features)
@@ -20,7 +20,10 @@ def linear_regression(features, values):
     result = model.fit()
     intercept = result.params[0]
     params = result.params[1:]
-
+    ###########################
+    ### YOUR CODE GOES HERE ###
+    ###########################
+    
     return intercept, params
 
 def predictions(dataframe):
@@ -28,24 +31,24 @@ def predictions(dataframe):
     The NYC turnstile data is stored in a pandas dataframe called weather_turnstile.
     Using the information stored in the dataframe, let's predict the ridership of
     the NYC subway using linear regression with gradient descent.
-
+    
     You can download the complete turnstile weather dataframe here:
-    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
-
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv    
+    
     Your prediction should have a R^2 value of 0.40 or better.
-    You need to experiment using various input features contained in the dataframe.
-    We recommend that you don't use the EXITSn_hourly feature as an input to the
-    linear model because we cannot use it as a predictor: we cannot use exits
-    counts as a way to predict entry counts.
-
+    You need to experiment using various input features contained in the dataframe. 
+    We recommend that you don't use the EXITSn_hourly feature as an input to the 
+    linear model because we cannot use it as a predictor: we cannot use exits 
+    counts as a way to predict entry counts. 
+    
     Note: Due to the memory and CPU limitation of our Amazon EC2 instance, we will
-    give you a random subet (~10%) of the data contained in
-    turnstile_data_master_with_weather.csv. You are encouraged to experiment with
+    give you a random subet (~10%) of the data contained in 
+    turnstile_data_master_with_weather.csv. You are encouraged to experiment with 
     this exercise on your own computer, locally. If you do, you may want to complete Exercise
     8 using gradient descent, or limit your number of features to 10 or so, since ordinary
     least squares can be very slow for a large number of features.
-
-    If you receive a "server has encountered an error" message, that means you are
+    
+    If you receive a "server has encountered an error" message, that means you are 
     hitting the 30-second limit that's placed on running your program. Try using a
     smaller number of features.
     '''
@@ -55,15 +58,16 @@ def predictions(dataframe):
     # See this page for more info about dummy variables:                                     #
     # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.get_dummies.html          #
     ##########################################################################################
-    features = dataframe[['Hour', 'mintempi', 'precipi', 'fog', 'rain', 'minpressurei', 'meanwindspdi']]
+    features = dataframe[['meantempi', 'rain', 'meanwindspdi']]
     dummy_units = pandas.get_dummies(dataframe['UNIT'], prefix='unit')
     features = features.join(dummy_units)
-
+    dummy_hours = pandas.get_dummies(dataframe['Hour'], prefix = 'hour')
+    feature = features.join(dummy_hours)
+    
     # Values
     values = dataframe['ENTRIESn_hourly']
 
     # Perform linear regression
     intercept, params = linear_regression(features, values)
-
     predictions = intercept + np.dot(features, params)
     return predictions
